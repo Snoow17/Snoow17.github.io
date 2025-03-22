f69d41f9a8faa3655372a1dbbf8ad87f151de19a
Snoow17 <j.rinzen@live.se>
2025-03-22 19:16:43 +0100

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -15,14 +15,14 @@
 
 <div class="email-form">
   <form ref="form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name" required>
-    <label>Email</label>
-    <input type="email" name="user_email" required>
-    <label>Message</label>
-    <textarea name="message" required></textarea>
-    <button class="submit-btn" type="submit">{{ buttonText }}</button>
-  </form>
+      <label>Name</label>
+      <input type="text" name="user_name" required />
+      <label>Email</label>
+      <input type="email" name="user_email" required />
+      <label>Message</label>
+      <textarea name="message" required></textarea>
+      <button class="submit-btn" type="submit">{{ buttonText }}</button>
+    </form>
 </div>
   </template>
 
